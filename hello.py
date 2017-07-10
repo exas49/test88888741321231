ca3efbf1a903cd4c4f57db363df39795eacb1202
exas49 <etupciauskas@mail.com>
2017-07-10 19:47:52 +0100

    Add files via upload
    My first python test

---
+++ hello.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+name = input("Give me your name: ")
+print("Your name is " +name)
+
+age = int(input("Enter your age: "))
+
+older = (str(2017-age+100))
+print("The year you will turn 100 is: " +older)
